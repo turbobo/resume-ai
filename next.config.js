d7c0a52cd3673a499f04dd7e53705a2703f8031a
qinghang <wb518737@cainiao.com>
2026-06-04 16:57:29 +0800

    add: EdgeOne Pages deployment config (edgeone.json + images unoptimized)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false
     return config
